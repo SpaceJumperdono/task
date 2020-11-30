00d365364a910325cc2b575f6e129d4fc86686f3
Pavel <pavel-laz2010@mail.ru>
2020-11-30 19:03:51 +0300

    delete check a negative number

--- lvl1.py
+++ lvl1.py
@@ -1,9 +1,6 @@
-def squirrel(int N):
-    if N > 0:
-        string_number = str(N)
-        lengh = len(string_number)
-        number = 10 ** (lengh - 1)
-        result = N // number
-        return result
-    else:
-        return 1
+def squirrel(N):
+    string_number = str(N)
+    lengh = len(string_number)
+    number = 10 ** (lengh - 1)
+    result = N // number
+    return result
