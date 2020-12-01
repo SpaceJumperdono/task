bcd020136661c70c90c7e6b7e8368cbfd5dec56b
Pavel <pavel-laz2010@mail.ru>
2020-12-01 09:08:26 +0300

    Add factorial calculation

--- lvl1.py
+++ lvl1.py
@@ -1,6 +1,11 @@
 def squirrel(N):
-    string_number = str(N)
+    factorial = 1
+    for i in range(1, N+1):
+        factorial *= i
+        print(factorial)
+    string_number = str(factorial)
     lengh = len(string_number)
     number = 10 ** (lengh - 1)
-    result = N // number
+    result = factorial // number
     return result
+print(squirrel(4))
