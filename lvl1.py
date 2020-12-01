33053a1d74881a6de445411237debd5d64aea1af
Pavel <pavel-laz2010@mail.ru>
2020-12-01 09:12:26 +0300

    clear function from print

--- lvl1.py
+++ lvl1.py
@@ -2,7 +2,6 @@ def squirrel(N):
     factorial = 1
     for i in range(1, N+1):
         factorial *= i
-        print(factorial)
     string_number = str(factorial)
     lengh = len(string_number)
     number = 10 ** (lengh - 1)
