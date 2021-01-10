6d9f5e048714f8d35c14ce128900beec8c5d9530
Pavel <pavel-laz2010@mail.ru>
2021-01-10 18:24:20 +0300

    creat two function for string and digits

--- lvl8.py
+++ lvl8.py
@@ -3,9 +3,10 @@ def score(lis):
     res = ''
     for i in a:
         res = res + ' ' + i
-    return int(len(res) - 1) 
+    return int(len(res) - 1)
+
 def stroke(l, string):
-    lengh = l
+    size = l
     string = string
     spisok = string.split(' ')
     print(spisok)
@@ -14,22 +15,57 @@ def stroke(l, string):
     i = 0
     j = 0
     while i < lengh:
-        example = a[j]
         check = score(a[j]) + len(str(spisok[i])) + 1
-        if(check <= 12):
+        if(check <= size):
             a[j].append(spisok[i])
         else:
             j += 1
             a.append([spisok[i]])
         i += 1
     return a
-def WordSearch(len, s, subs):
-    spisok = stroke(len, s)
-    line = []
-    for i in spisok:
-        if(subs in i):
-            line.append(1)
+
+def score_line(lis):
+    a = lis
+    res = ''
+    for i in a:
+        res = res + i
+    return int(len(res)) 
+
+def linez(size, string):
+    size = size
+    spisok = string
+    a = [[]]
+    lengh = len(str(spisok))
+    i = 0
+    j = 0
+    while i < lengh:
+        check = score_line(a[j]) + len(spisok[i])
+        if(check <= size):
+            a[j].append(spisok[i])
         else:
-            line.append(0)
-    return line      
-    
+            j += 1
+            a.append([spisok[i]])
+        i += 1
+    return a
+
+def WordSearch(len, s, subs):
+    if(' ' in s):
+        spisok = stroke(len, s)
+        line = []
+        for i in spisok:
+            b = ''.join(i)
+            if(subs in i):
+                line.append(1)
+            else:
+                line.append(0)
+        return line
+    else:
+        spisok = linez(len, s)
+        line = []
+        for i in spisok:
+            b = ''.join(i)
+            if(subs in b):
+                line.append(1)
+            else:
+                line.append(0)
+        return line
