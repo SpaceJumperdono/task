4c52730c124f0270abce265321ae27c404db1346
pavel mikheev <pavel-laz2010@mail.ru>
2021-01-25 00:43:04 +0300

    add check string

--- lvl12.py
+++ lvl12.py
@@ -23,11 +23,19 @@ def parse(s):
     return result
     
 def BigMinus(s1, s2):
-    a = s1[::-1]
-    b = s2[::-1]
+    a = s1
+    b = s2
     t = 0
     shift = 0
     result = ""
+    if (len(a) == len(b)):
+        if(a[0] < b[0]):
+            a, b = b, a
+    else:
+        if(len(a) < len(b)):
+            a, b = b, a
+    a = a[::-1]
+    b = b[::-1]
     for i in range(len(a)):
         if(i < len(b)):
             t = parse(a[i]) - parse(b[i]) - shift
@@ -35,6 +43,7 @@ def BigMinus(s1, s2):
         else:
             t = parse(a[i]) - shift
             shift = 0
+        
         if(t < 0):
             t += 10
             shift = 1
