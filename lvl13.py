1c68b542571f4ab0649c05572d3eb3e3f3df633a
pavel mikheev <pavel-laz2010@mail.ru>
2021-01-25 15:38:28 +0300

    add lvl13.py

---
+++ lvl13.py
@@ -0,0 +1,30 @@
+def MassVote(N, Votes):
+    N = N
+    Votes = Votes
+    max = Votes[0]
+    summa = 0
+    flag = False
+    position = 1
+    for i in range(1, len(Votes)):
+        if(max < Votes[i]):
+            max = Votes[i]
+            position = i + 1
+            continue
+        if(max == Votes[i]):
+            flag = True
+            break
+    for i in Votes:
+        summa += i
+    result = 0
+
+    if(flag == False):
+        result = (max * 100) / 100
+        if(result > 50):
+            string = "majority winner " + str(position)
+            return string
+        if(result < 50):
+            string = "minority winner " + str(position)
+            return string
+    else:
+        string = "no winner"
+        return string
